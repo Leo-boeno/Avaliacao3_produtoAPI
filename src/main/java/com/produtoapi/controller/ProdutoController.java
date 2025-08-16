29baba90703c404a1291fce9d2e25cbb1e748bbf
Cristiano <cristianonetto@gmail.com>
2025-08-16 11:15:21 -0300

    Envio de arquivos ProdutoAPI

---
+++ src/main/java/com/produtoapi/controller/ProdutoController.java
@@ -0,0 +1,47 @@
+package com.produtoapi.controller;
+
+import com.produtoapi.model.Produto;
+import com.produtoapi.service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+    @Autowired
+    private ProdutoService produtoService;
+
+    @GetMapping
+    public List<Produto> listarTodos(){
+        return produtoService.listarTodos();
+    }
+
+    @PostMapping
+    public Produto salvar(@RequestBody Produto produto){
+        return produtoService.salvar(produto);
+    }
+
+    @PutMapping("/{id}")
+    public Produto atualizar(@PathVariable Long id, @RequestBody Produto produto){
+        return produtoService.atualizar(id, produto);
+    }
+
+    @PatchMapping("/{id}")
+    public Produto atualizarParcialmente(@PathVariable Long id, @RequestBody Produto produtoParcial) {
+        return produtoService.atualizarParcialmente(id, produtoParcial);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id){
+        produtoService.deletar(id);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Produto> findById(@PathVariable Long id){
+        return produtoService.findById(id);
+    }
+}
